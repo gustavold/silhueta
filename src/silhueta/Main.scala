920cea0d2101347c7eb5b3520749bd7322a1fe1c
Marcio Vinicius Santos <marcioviniciussantos@marcio-vinicius-santoss-macbook-pro.local>
2009-11-24 23:29:02 -0200

    Adicionado gerador de lista de silhuetas

--- src/silhueta/Main.scala
+++ src/silhueta/Main.scala
@@ -15,6 +15,8 @@ object Main {
   def main(args: Array[String]) :Unit = {
     println("Hello, world!")
     ManipuladorDeArquivos.lerArquivoDeEntrada("/Users/marcioviniciussantos/Documents/Projects/silhueta/exemplo_entrada.txt")
+    ManipuladorDeArquivos.escreveArquivoDeSilhueta("/Users/marcioviniciussantos/Documents/Projects/silhueta/teste_saida.txt", 
+      List(ElemSilhueta(1,  11),ElemSilhueta(3,  13),ElemSilhueta(9,  0),ElemSilhueta(12,  7),ElemSilhueta(16,  3),ElemSilhueta(19,  18),ElemSilhueta(22,  3),ElemSilhueta(23,  13),ElemSilhueta(29,  0)))
   }
 
 }
